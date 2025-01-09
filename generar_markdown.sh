edb2a952af7fce237e12b5239c304e9d4d9e5e9a
John Doe <johndoe@example.com>
2025-01-09 10:51:46 -0500

    Update mardown

--- generar_markdown.sh
+++ generar_markdown.sh
@@ -20,15 +20,20 @@ echo "" >> $OUTPUT_FILE
 if git rev-parse "$TAG2" >/dev/null 2>&1; then
   # Si ambos tags existen, se muestran los commits entre ellos
   echo "Mostrando commits entre $TAG1 y $TAG2..." >> $OUTPUT_FILE
-  git log $TAG1..$TAG2 --oneline | while read commit; do
-    echo "- $commit" >> $OUTPUT_FILE
-  done
+  git log $TAG1..$TAG2 --oneline --pretty=format:"- %s" >> $OUTPUT_FILE
 else
   # Si el segundo tag no existe, mostrar los commits desde el inicio del repo hasta el primer tag
   echo "El tag $TAG2 no existe, mostrando commits hasta $TAG1..." >> $OUTPUT_FILE
-  git log --oneline $TAG1 | while read commit; do
-    echo "- $commit" >> $OUTPUT_FILE
-  done
+  git log --oneline $TAG1 --pretty=format:"- %s" >> $OUTPUT_FILE
 fi
+
+# Agregar título de Contribuidores
+echo "" >> $OUTPUT_FILE
+echo "## Contribuidores:" >> $OUTPUT_FILE
+
+# Obtener la lista de contribuidores (autores de los commits)
+git log $TAG1..$TAG2 --pretty=format:"%an" | sort | uniq | while read contributor; do
+  echo "- $contributor" >> $OUTPUT_FILE
+done
 
 echo "Archivo $OUTPUT_FILE generado con éxito."
